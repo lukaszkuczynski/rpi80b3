2dfbb4da40f51bdfeb0685e7e4093b42bf7a0856
lukaszkuczynski <kuczynskilukasz@gmail.com>
2021-01-08 17:35:10 +0100

    PWM on RPi Zero

---
+++ pwmtest.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(12, GPIO.OUT)
+ 
+led1 = GPIO.PWM(12, 50)
+df = 0
+led1.start(df)
+ 
+try:
+    while True:
+        for dc in range(0,101,5):
+            led1.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+        for dc in range(100,0,-5):
+            led1.ChangeDutyCycle(dc)
+            time.sleep(0.02)
+except KeyboardInterrupt:
+    print('End of PWM-ing')
+    led1.stop()
+    GPIO.cleanup()
